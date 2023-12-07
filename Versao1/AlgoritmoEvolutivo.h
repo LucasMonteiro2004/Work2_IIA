04c510174299b9de45b6f64e8b37bca1aa9fe56b
Lucas <lucasgmonteiro96@gmail.com>
2023-12-07 15:06:53 +0000

    update Local search

--- Versao1/AlgoritmoEvolutivo.h
+++ Versao1/AlgoritmoEvolutivo.h
@@ -3,4 +3,7 @@
 
 #include "PesquisaLocal.h"
 
+int *algoritmoRecombinacao(int *melhorSolucoa, Grafo *grafo, Edge **edges, int *k);
+int *algorimoMutacao(int *melhorSolucoa, Grafo *grafo, Edge **edges, int *k);
+
 #endif //VERSAO1_ALGORITMOEVOLUTIVO_H
