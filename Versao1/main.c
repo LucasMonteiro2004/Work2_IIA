c5c46f07754323259f5c275e459d86be1c57fa49
Lucas <lucasgmonteiro96@gmail.com>
2023-12-15 20:34:13 +0000

    update erro resolvido

--- Versao1/main.c
+++ Versao1/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "PesquisaLocal.h"
 
 #define INTER 300
@@ -6,10 +9,11 @@
 int main() {
     srand(time(NULL));
     int k, numVertices, numEdges;
-    int *sub;
+    int *sub, *melhorSub;
     Edge *edges;
     Grafo *grafo;
-    Resultado* sol;
+    Resultado *sol;
+    int melhorCusto = INT_MAX;
 
     printf("Nome arquivo?");
     char arquivo[20];
@@ -26,20 +30,29 @@ int main() {
 
     sub = geraSolucaoInicial(&k, &edges, &grafo);
     imprimirSubconjunto(sub, grafo->numVertices);
+    melhorSub = malloc(grafo->numVertices * sizeof(int));
+    memcpy(melhorSub, sub, grafo->numVertices * sizeof(int));
 
     for(int i = 0; i < INTER; i++){
         sol = Hill_Climbing_2(sub, grafo, &edges, &k);
         imprimirSubconjunto(sol->melhorSolucao, grafo->numVertices);
         validateSoluction(sol, grafo, &edges, &k);
+        int custoAtual = calculaCustoTotal(sol->melhorSolucao, &edges, grafo->numArestas);
+
+        if (custoAtual < melhorCusto) {
+            melhorCusto = custoAtual;
+            memcpy(melhorSub, sol->melhorSolucao, grafo->numVertices * sizeof(int));
+        }
 
         memcpy(sub, sol->melhorSolucao, grafo->numVertices * sizeof(int));
-
         free(sol);
     }
+
+    printf("Melhor solucao encontrada com custo: %d\n", melhorCusto);
+    imprimirSubconjunto(melhorSub, grafo->numVertices);
 
     free(edges);
     free(grafo);
     free(sub);
-
     return 0;
 }
