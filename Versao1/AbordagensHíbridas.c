dbdff757822918cefbf4122746c92d2830da33e6
Lucas <lucasgmonteiro96@gmail.com>
2023-12-17 13:37:32 +0000

    finalizando o trabalho

--- Versao1/AbordagensHíbridas.c
+++ Versao1/AbordagensHíbridas.c
@@ -3,35 +3,46 @@
 int* Hibrido(int k, Edge **edges, Grafo *grafo, int interacoes, int* solucaoInicial) {
     int *melhorSolucao = malloc(grafo->numVertices * sizeof(int));
     if (!melhorSolucao) {
-        printf("Erro na alocacaoo de memoria para melhorSolucao.\n");
+        printf("Erro na alocacao de memoria para melhorSolucao.\n");
         return NULL;
     }
-
     memcpy(melhorSolucao, solucaoInicial, grafo->numVertices * sizeof(int));
     int melhorCusto = INT_MAX;
 
     for (int i = 0; i < interacoes; i++) {
-        Resultado *resultadoFinal = Hill_Climbing_2(melhorSolucao, grafo, edges, &k);
-        if (!resultadoFinal) {
-            printf("Erro em Hill_Climbing_2.\n");
+        // Aplica a mutação por inserção
+        Resultado *resultadoMutacao = algoritmoMutacao_Insercao(melhorSolucao, grafo, edges, &k);
+        if (!resultadoMutacao) {
+            printf("Erro em algoritmoMutacao_Insercao.\n");
             free(melhorSolucao);
             return NULL;
         }
 
-        if (validateSoluction(resultadoFinal, grafo, edges, &k)) {
-            int custoAtual = calculaCustoTotal(resultadoFinal->melhorSolucao, edges, grafo->numArestas);
-            if (custoAtual < melhorCusto) {
-                melhorCusto = custoAtual;
-                memcpy(melhorSolucao, resultadoFinal->melhorSolucao, grafo->numVertices * sizeof(int));
-            }
+        // Aplica Hill Climbing na solução mutada
+        Resultado *resultadoHillClimbing = Hill_Climbing(resultadoMutacao->melhorSolucao, grafo, edges, &k);
+        if (!resultadoHillClimbing) {
+            printf("Erro em Hill_Climbing.\n");
+            free(resultadoMutacao->melhorSolucao);
+            free(resultadoMutacao);
+            free(melhorSolucao);
+            return NULL;
         }
 
-        free(resultadoFinal->melhorSolucao);
-        free(resultadoFinal);
+        int custoAtual = calculaCustoTotal(resultadoHillClimbing->melhorSolucao, edges, grafo->numArestas);
+        if (custoAtual < melhorCusto) {
+            melhorCusto = custoAtual;
+            memcpy(melhorSolucao, resultadoHillClimbing->melhorSolucao, grafo->numVertices * sizeof(int));
+        }
+
+        free(resultadoMutacao->melhorSolucao);
+        free(resultadoMutacao);
+        free(resultadoHillClimbing->melhorSolucao);
+        free(resultadoHillClimbing);
     }
 
     return melhorSolucao;
 }
+
 
 int* Hibrido_2(int k, Edge **edges, Grafo *grafo, int interacoes, int* solucaoInicial) {
     int *melhorSolucao = malloc(grafo->numVertices * sizeof(int));
